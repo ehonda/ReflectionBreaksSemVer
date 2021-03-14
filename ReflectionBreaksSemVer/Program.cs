b54133f532d72a5c7868e40c928db6ac83d4f275
Dennis Renz <dennis@r-renz.de>
2021-03-14 15:15:20 +0100

    Set up methods broken by additional features

--- ReflectionBreaksSemVer/Program.cs
+++ ReflectionBreaksSemVer/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace ReflectionBreaksSemVer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void PrintLibraryInfo()
         {
             var library = Assembly.LoadFrom("SomeLib.dll");
 
@@ -19,6 +20,28 @@ namespace ReflectionBreaksSemVer
                     Console.WriteLine($"{method.Name}");
                 }
             }
+        }
+
+        static bool BrokenByAdditionalType()
+            => Assembly
+                .LoadFrom("SomeLib.dll")
+                .ExportedTypes
+                .Count() == 1;
+
+        static bool BrokenByAdditionalMethod()
+            => Assembly
+                .LoadFrom("SomeLib.dll")
+                .ExportedTypes
+                .Single(type => type.Name == "ClassA")
+                .GetMethods()
+                .Length == 5;
+
+        static void Main(string[] args)
+        {
+            PrintLibraryInfo();
+            Console.WriteLine();
+            Console.WriteLine($"{BrokenByAdditionalType()}");
+            Console.WriteLine($"{BrokenByAdditionalMethod()}");
         }
     }
 }
