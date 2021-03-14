8e05bc89d23d5644fcb1ec38b74534f79217ea43
Dennis Renz <dennis@r-renz.de>
2021-03-14 15:09:15 +0100

    Add missing components

---
+++ ReflectionBreaksSemVer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionBreaksSemVer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = Assembly.LoadFrom("SomeLib.dll");
+
+            var types = library.ExportedTypes;
+
+            foreach (var type in types)
+            {
+                Console.WriteLine($"{type.FullName}");
+                foreach (var method in type.GetMethods())
+                {
+                    Console.WriteLine($"{method.Name}");
+                }
+            }
+        }
+    }
+}
